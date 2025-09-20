8ab928249723071fa5ba5366d9d84ea6ab9052d3
Diego Arango <diego_arango7@hotmail.com>
2025-09-20 14:57:25 -0500

    update, se realiza validación y creación de expresiones

---
+++ helpers/expresiones.js
@@ -0,0 +1,4 @@
+export let expresiones = {
+    texto:/^[a-zA-ZáéíóúüñÁÉÍÓÚÜÑ\s-]{5,50}$/,
+    numero:/^\d{5,10}$/
+}
--- helpers/validarRegistro.js
+++ helpers/validarRegistro.js
@@ -1,6 +1,36 @@
+import { expresiones } from'./expresiones.js'
 
+let inputs = document.getElementsByClassName("formulario__entrada");
+let arregloInputs = [...inputs];
+arregloInputs.map(input => {
+    input.addEventListener("keyup", (e) => {
+        switch (e.target.id){
+            case "nombre":
+                if(expresiones.texto.test(e.target.value)){
+                    e.target.style.borderColor = "green"
+                } else{
+                      document.getElementById("nombre").style.borderColor = "red"
+                }
+                break;
+            case "apellido":
+                if(expresiones.texto.test(e.target.value)){
+                    e.target.classList.add("correcto")
+                    e.target.classList.remove("incorrecto")
+                } else {
+                    e.target.classList.add("incorrecto")
+                    e.target.classList.remove("correcto")
+                }
+                break;
+                 case "documento":
+                if(expresiones.numero.test(e.target.value)){
+                    e.target.classList.add("correcto")
+                    e.target.classList.remove("incorrecto")
+                } else {
+                    e.target.classList.add("incorrecto")
+                    e.target.classList.remove("correcto")
+                }
+                break;
+        }
 
-let inputs = document.getElementsByClassName ("formulario_entrada")
-let arregloInputs= [...inputs]
-console.log(inputs)
-console.log(arregloInputs)
+    });
+})
